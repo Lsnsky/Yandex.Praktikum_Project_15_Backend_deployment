0efec4d3882b386617d14ee96f17827d59dff69e
Igor Lisyanskiy <lsnsky@pm.me>
2020-05-26 23:34:50 +0300

    refactor:подключить правильно переменные окружения

--- controllers/users.js
+++ controllers/users.js
@@ -7,86 +7,86 @@ const BadRequestError = require('../errors/bad-request-err');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports.getUsers = (req, res, next) => {
-  User.find({})
-    .then((user) => {
-      res.send({ data: user });
-    })
-    .catch(next);
+    User.find({})
+        .then((user) => {
+            res.send({ data: user });
+        })
+        .catch(next);
 };
 
 module.exports.getUserById = (req, res, next) => {
-  User.findById(req.params.id)
-    .orFail(() => new NotFoundError('Нет пользователя с таким id'))
-    .then((user) => {
-      res.send({ data: user });
-    })
-    .catch(next);
+    User.findById(req.params.id)
+        .orFail(() => new NotFoundError('Нет пользователя с таким id'))
+        .then((user) => {
+            res.send({ data: user });
+        })
+        .catch(next);
 };
 
 module.exports.createUser = (req, res, next) => {
-  const {
-    name,
-    about,
-    avatar,
-    email,
-  } = req.body;
-  bcrypt.hash(req.body.password, 10)
-    .then((hash) => User.create({
-      name,
-      about,
-      avatar,
-      email,
-      password: hash,
-    }))
+    const {
+        name,
+        about,
+        avatar,
+        email,
+    } = req.body;
+    bcrypt.hash(req.body.password, 10)
+        .then((hash) => User.create({
+            name,
+            about,
+            avatar,
+            email,
+            password: hash,
+        }))
 
     .then((user) => {
-      if (!user) {
-        throw new BadRequestError('Ошибка при отправке запроса');
-      }
-      res.send(user.omitPrivate({ data: user }));
-      // res.send({ data: user });
-    })
-    .catch(next);
+            if (!user) {
+                throw new BadRequestError('Ошибка при отправке запроса');
+            }
+            res.send(user.omitPrivate({ data: user }));
+            // res.send({ data: user });
+        })
+        .catch(next);
 };
 
 // обновление профиля пользователя
 module.exports.updateUserProfile = (req, res, next) => {
-  const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, {
-    new: true, // обработчик then получит на вход обновлённую запись
-    runValidators: true, // данные будут валидированы перед изменением
-    upsert: false, // если пользователь не найден, он не будет создан
-  })
-    .then((user) => {
-      res.send({ data: user });
-    })
-    .catch(next);
+    const { name, about } = req.body;
+    User.findByIdAndUpdate(req.user._id, { name, about }, {
+            new: true, // обработчик then получит на вход обновлённую запись
+            runValidators: true, // данные будут валидированы перед изменением
+            upsert: false, // если пользователь не найден, он не будет создан
+        })
+        .then((user) => {
+            res.send({ data: user });
+        })
+        .catch(next);
 };
 
 // обновление аватара пользователя
 module.exports.updateUserAvatar = (req, res, next) => {
-  const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }, {
-    new: true, // обработчик then получит на вход обновлённую запись
-    runValidators: true, // данные будут валидированы перед изменением
-    upsert: false, // если пользователь не найден, он не будет создан
-  })
-    .then((user) => {
-      res.send({ data: user });
-    })
-    .catch(next);
+    const { avatar } = req.body;
+    User.findByIdAndUpdate(req.user._id, { avatar }, {
+            new: true, // обработчик then получит на вход обновлённую запись
+            runValidators: true, // данные будут валидированы перед изменением
+            upsert: false, // если пользователь не найден, он не будет создан
+        })
+        .then((user) => {
+            res.send({ data: user });
+        })
+        .catch(next);
 };
 
 module.exports.login = (req, res, next) => {
-  const { email, password } = req.body;
-  return User.findUser(email, password)
-    .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-      res.send({ token });
-      res.cookie('jwt', token, {
-        maxAge: 3600000 * 24 * 7,
-        httpOnly: true,
-      });
-    })
-    .catch(next);
+    const { email, password } = req.body;
+    return User.findUser(email, password)
+        .then((user) => {
+            const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
+            return (res.send({ token }) ||
+                res.cookie('jwt', token, {
+                    maxAge: 3600000 * 24 * 7,
+                    httpOnly: true,
+                }));
+        })
+        .catch(next);
 };
